8fcb44fed5080646e6811962189af97a7db36ef8
NatashaChabak <nc1chabak@gmail.com>
2023-03-27 19:02:42 +0300

    "Тестовое ДЗ к BaseJava, попытка 2"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int size = 0;
+    private int size = 1;
 
     void clear() {
         for (int i = 0; i < size; i++) {
